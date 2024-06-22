3cdf46619ccbf1e2d12e159949843624c2654011
agun214 <gunder214@gmail.com>
2024-06-21 20:00:54 -0500

    Create folder for new main.c

---
+++ gamepad_alsa_midi/main.c
@@ -0,0 +1,19 @@
+#include "gamepad_alsa_midi_functions.h"
+
+/* compile instructions: 
+gcc -c gamepad_alsa_midi_functions.c -o gamepad_alsa_midi_functions.o
+gcc -o gamepad_alsa_midi main.c gamepad_alsa_midi_functions.o -I/usr/include/libevdev-1.0 -levdev -lasound 
+*/
+
+int main(int argc, char **argv) {
+    int fd, rc, port;
+	snd_seq_t* midi;
+	struct libevdev* dev = ps3_connect(&fd, &rc);
+	setup_midi_port(&midi, &port);
+	dev_midi_event_loop(dev, midi, port);
+
+    // Clean up resources
+	libevdev_free(dev);
+	close(fd);
+	return 0;
+}
